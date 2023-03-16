435c8224f2f660cfbab08acabdb68b471176b25e
tokitaka <artigen3@gmail.com>
2023-03-17 01:37:05 +0900

    component scan method version

--- src/main/java/shop/mtcoding/reflection_study/MyRefApp.java
+++ src/main/java/shop/mtcoding/reflection_study/MyRefApp.java
@@ -1,22 +1,54 @@
 package shop.mtcoding.reflection_study;
-
+import java.io.File;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
+import org.springframework.stereotype.Controller;
 
 import shop.mtcoding.reflection_study.anno.RequestMapping;
 
 public class MyRefApp {
-    public static void findUri(UserController uc, String uri) throws Exception{
-        Boolean isFind = false;
-        Method[] methods = uc.getClass().getDeclaredMethods();
+    private static Set<Class> ComponentScan(String pkg) throws Exception{
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Set<Class> classes = new HashSet<>(); // initial set
 
-        for (Method mt : methods) {
-            Annotation anno = mt.getDeclaredAnnotation(RequestMapping.class);
-            RequestMapping rm = (RequestMapping) anno;
-            if(rm.uri().equals(uri)){
-                isFind = true;
-                mt.invoke(uc);
+        URL pacakageUrl = classLoader.getResource(pkg);
+        File packageDirectory = new File(pacakageUrl.toURI());
+        for (File file : packageDirectory.listFiles()) {
+            if (file.getName().endsWith(".class")){
+                String className = pkg + "." + file.getName().replace("class","");
+                Class cls = Class.forName(className);
+                classes.add(cls); // list 에 클래스 추가 
+            }
+        }
+
+        return classes;
+    }
+
+    public static void findUri(Set<Class> classes, String uri) throws Exception{
+        boolean isFind = false; // initial set
+        // finding classes, methods with the annotation
+        for (Class cls : classes) {
+            if (cls.isAnnotationPresent(Controller.class)){
+               Object instance = cls.newInstance();
+               Method[] methods = cls.getDeclaredMethods();
+
+               for (Method mt : methods) {
+                   Annotation anno = mt.getDeclaredAnnotation(RequestMapping.class);
+                   RequestMapping rm = (RequestMapping) anno;
+                   if(rm.uri().equals(uri)){
+                       isFind = true;
+                       mt.invoke(instance);
+            }
+        }
+        if (isFind == false){
+            System.out.println("404 not found");
+        }
+
                 
             }
         }
@@ -24,7 +56,7 @@ public class MyRefApp {
     public static void main(String[] args) throws Exception {
        Scanner sc = new Scanner(System.in); 
        String uri = sc.nextLine();
-
-       findUri(new UserController(), uri);
+        Set<Class> classes = ComponentScan("reflection_study");
+       findUri(classes, uri);
     }
 }
--- src/main/java/shop/mtcoding/reflection_study/UserController.java
+++ src/main/java/shop/mtcoding/reflection_study/UserController.java
@@ -1,9 +1,10 @@
 package shop.mtcoding.reflection_study;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 public class UserController {
     @RequestMapping(uri="/login")
     public void login(){
